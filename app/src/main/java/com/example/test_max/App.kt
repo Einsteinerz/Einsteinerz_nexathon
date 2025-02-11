a05672a65560071d31dd3aeb51563b3c5dc74556
Vishal <230638.EC@rmkec.ac.in>
2025-02-11 12:05:52 +0530

    Final commit

---
+++ app/src/main/java/com/example/test_max/App.kt
@@ -0,0 +1,10 @@
+package com.example.test_max
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize any app-wide configurations here
+    }
+} 
